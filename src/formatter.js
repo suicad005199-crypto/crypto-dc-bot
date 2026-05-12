d6112ac3b8bba52f0aac433b699496ef7bb82092
suicad005199-crypto <suicad005199@gmail.com>
2026-05-12 15:21:17 +0800

    Update formatter.js

--- src/formatter.js
+++ src/formatter.js
@@ -1,1 +1,25 @@
-
+{
+  "version": "0.2.0",
+  "configurations": [
+    {
+      "type": "node",
+      "request": "launch",
+      "name": "🚀 啟動 Discord Bot",
+      "program": "${workspaceFolder}/src/index.js",
+      "envFile": "${workspaceFolder}/.env",
+      "console": "integratedTerminal",
+      "restart": true,
+      "runtimeExecutable": "node"
+    },
+    {
+      "type": "node",
+      "request": "launch",
+      "name": "🔧 開發模式 (nodemon)",
+      "runtimeExecutable": "${workspaceFolder}/node_modules/.bin/nodemon",
+      "program": "${workspaceFolder}/src/index.js",
+      "envFile": "${workspaceFolder}/.env",
+      "console": "integratedTerminal",
+      "restart": true
+    }
+  ]
+}
